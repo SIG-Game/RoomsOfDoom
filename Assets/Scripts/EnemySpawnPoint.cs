2617e6af75cb08da2b6aadcf64f04acba92ae920
eky2429 <eky2429@gmail.com>
2024-10-12 16:50:31 -0500

    Revert "Reapply "Merge pull request #16 from SIG-Game/revert-15-Speedrun_Branch""
    This reverts commit 386a37e901220fcb7285e2694ba254dfbf003763.

---
+++ Assets/Scripts/EnemySpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+    public GameObject enemy;
+
+    public void Spawn()
+    {
+        if (transform.childCount < 3)
+        {
+            Instantiate(enemy, transform);
+        }
+    }
+}
